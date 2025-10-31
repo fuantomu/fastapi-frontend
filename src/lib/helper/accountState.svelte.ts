1beff918ff3a702f111249a56f638b1a0f56447a
fuantomu <fuantomuw@gmail.com>
2025-10-31 20:45:44 +0100

    fixed state reactivity

--- src/lib/helper/accountState.svelte.ts
+++ src/lib/helper/accountState.svelte.ts
@@ -2,7 +2,5 @@ import type { Account } from "$lib/types";
 
 export function createAccountState(_account: Account) : Account {
   let account = $state(_account)
-  return {
-    ...account
-  }
+  return account
 }
